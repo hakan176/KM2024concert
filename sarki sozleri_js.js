dedd4bdd963982477871fa94ef13b239005f7480
hakan karaca <31946534+hakan176@users.noreply.github.com>
2024-02-02 08:24:54 -0500

    Add files via upload

---
+++ sarki sozleri_js.js
@@ -0,0 +1,312 @@
+
+
+
+/*    Song01    */
+let song01_title = "<h3><span id=''><strong>1.Şarkı - Sevemedim Kara Gözlüm</strong></span><span style='font-size:40px; border:2px solid black;'><strong><strong>&#43; &#150;</strong></strong></span></h3>";
+document.getElementById("div_song01_title").innerHTML = song01_title;
+ 
+let song01_lyrics = "<pre>Sevemedim kara gözlüm, seni doyunca<span id='a'><br>I couldn't love enough of you, my dark-eyed one</span></pre>"
++ "<pre>Hep kıskandım seni elden, yıllar boyunca<span id='a'><br>I've always envied you from others, over the years</span></pre><br>"
+
++ "<pre>Kuşlar gibi ikimiz bir, yuva kuralım<span id='a'><br>Let two of us build a nest, like birds</span></pre>"
++ "<pre>Ayırmasın mevlam bizi, ömür boyunca<span id='a'><br>May God not separate us, for all eternity</span></pre><br>"
+
++ "<pre>Aramıza kimse, gelip girmesin<span id='a'><br>Let no one come between us</span></pre>"
++ "<pre>Ayırmasın mevlam bizi, ömür boyunca<span id='a'><br>May God not separate us, for all eternity</span></pre>"
++ "<pre>Aramıza kimse, gelip girmesin<span id='a'><br>Let no one come between us</span></pre>"
++ "<pre>Ayırmasın mevlam bizi, ömür boyunca<span id='a'><br>May God not separate us, for all eternity</span></pre><br>"
+
++ "<p id='aranagme'>ARANAĞME<br><span></span></p><br>"
+
++ "<pre>Bana cefa ediyorlar, bilmem nedendir<span id='a'><br>They are making me suffer, i don’t know why</span></pre>"
++ "<pre>Benim korkum senden değil, kaderimdendir<span id='a'><br>The source of my fear is not you, but it is my fate</span></pre><br>"
+
++ "<pre>Herkes bana deli diye, gülüp geçiyor<span id='a'><br>Everyone laughs at me, calling me crazy</span></pre>"
++ "<pre>Senin aşkın beni kara gözlüm, deli ediyor<span id='a'><br>It is your love, my dark-eyed one, that drives me crazy</span></pre><br>"
+
++ "<pre>Aramıza kimse, gelip girmesin<span id='a'><br>Let no one come between us</span></pre>"
++ "<pre>Ayırmasın mevlam bizi, ömür boyunca<span id='a'><br>May God not separate us, for all eternity</span></pre>"
++ "<pre>Aramıza kimse, gelip girmesin<span id='a'><br>Let no one come between us</span></pre>"
++ "<pre>Ayırmasın mevlam bizi, ömür boyunca<span id='a'><br>May God not separate us, for all eternity</span></pre>"
++ "<pre>Ayırmasın mevlam bizi, ömür boyunca<span id='a'><br>May God not separate us, for all eternity</span></pre><br>";
+
+document.getElementById("div_song01_lyrics").innerHTML = song01_lyrics;
+
+
+/*    Song02    */
+let song02_title = '<h3><strong>2.Şarkı - Benim Gözüm Sende</strong></h3>';
+document.getElementById("div_song02_title").innerHTML = song02_title;
+
+let song02_lyrics = "<pre>Görünce aşık oldum<span id='a'><br>I fell in love when I saw</span></pre>"
++ "<pre>O güzel gözlerine<span id='a'><br>Those beautiful eyes of yours</span></pre>"
++ "<pre>Başkasını istemem<span id='a'><br>I don't want anyone else</span></pre>"
++ "<pre>Benim gözüm sende<span id='a'><br>My eyes are on you</span></pre><br>"
+
++ "<pre>Görünce aşık oldum<span id='a'><br>I fell in love when I saw</span></pre>"
++ "<pre>O güzel gözlerine<span id='a'><br>Those beautiful eyes of yours</span></pre>"
++ "<pre>Başkasını istemem<span id='a'><br>I don't want anyone else</span></pre>"
++ "<pre>Benim gözüm sende<span id='a'><br>My eyes are on you</span></pre><br>"
+
++ "<p id='aranagme'>ARANAĞME<br><span></span></p><br>"
+
++ "<pre>Yalvarırım ne olur<span id='a'><br>I beg you, please</span></pre>"
++ "<pre>Başka birini sevme<span id='a'><br>Don't love someone else</span></pre>"
++ "<pre>Ben sensiz yaşayamam<span id='a'><br>I can't live without you</span></pre>"
++ "<pre>Benim gözüm sende<span id='a'><br>My eyes are on you</span></pre><br>"
+
++ "<pre>Yalvarırım ne olur<span id='a'><br>I beg you, please</span></pre>"
++ "<pre>Başka birini sevme<span id='a'><br>Don't love someone else</span></pre>"
++ "<pre>Ben sensiz yaşayamam<span id='a'><br>I can't live without you</span></pre>"
++ "<pre>Benim gözüm sende<span id='a'><br>My eyes are on you</span></pre><br>"
+
++ "<p id='aranagme'>ARANAĞME<br><span></span></p><br>"
+
++ "<pre>Ömür boyu beklerim<span id='a'><br>I'll wait for a lifetime</span></pre>"
++ "<pre>Belki seversin diye<span id='a'><br>Maybe you'll love me</span></pre>"
++ "<pre>Başkasını istemem<span id='a'><br>I don't want anyone else</span></pre>"
++ "<pre>Benim gözüm sende<span id='a'><br>My eyes are on you</span></pre><br>"
+
++ "<pre>Ömür boyu beklerim<span id='a'><br>I'll wait for a lifetime</span></pre>"
++ "<pre>Belki seversin diye<span id='a'><br>Maybe you'll love me</span></pre>"
++ "<pre>Başkasını istemem<span id='a'><br>I don't want anyone else</span></pre>"
++ "<pre>Benim gözüm sende<span id='a'><br>My eyes are on you</span></pre>"
++ "<pre>Benim gözüm sende<span id='a'><br>My eyes are on you</span></pre>"
++ "<pre>Benim gözüm sende<span id='a'><br>My eyes are on you</span></pre><br>";
+
+document.getElementById("div_song02_lyrics").innerHTML = song02_lyrics;
+
+
+/*    Song03    */
+let song03_title = '<h3><strong>3.Şarkı - Bir Garip Yolcuyum</strong></h3>';
+document.getElementById("div_song03_title").innerHTML = song03_title;
+
+let song03_lyrics = "<pre>Rüyalar gerçek olsa, seni hergün görürdüm<span id='a'><br>If dreams came true, I would see you every day</span></pre>"
++ "<pre>O incecik beline, sarılarak yürürdüm<span id='a'><br>I would walk by hugging that thin waist of yours</span></pre>"
++ "<pre>Rüyalar gerçek olsa, seni hergün görürdüm<span id='a'><br>If dreams came true, I would see you every day</span></pre>"
++ "<pre>O incecik beline, sarılarak yürürdüm<span id='a'><br>I would walk by hugging that thin waist of yours</span></pre><br>"
+
++ "<pre>Sabah olmasın diye güneşi durdururdum<span id='a'><br>I would stop sun rising so that there would be no morning</span></pre>"
++ "<pre>Yanar dağlarda tüten ateşi söndürürdüm<span id='a'><br>I would extinguish the fire burning in the volcanoes</span></pre>"
++ "<pre>Yatağına her gece gelincik doldururdum<span id='a'><br>I would fill your bed with poppies every night</span></pre>"
++ "<pre>Dudağına bin kere öpücük kondururdum<span id='a'><br>I would kiss your lips a thousand times</span></pre><br>"
+
++ "<pre>Rüyalar, rüyalar<span id='a'><br>Dreams, dreams</span></pre>"
++ "<pre>Rüyalaaar, rüyalar gerçek olsa<span id='a'><br>Dreeeaams, if dreams came true</span></pre><br>"
+
++ "<p id='aranagme'>ARANAĞME<br><span></span></p><br>"
+
++ "<pre>Rüyalar gerçek olsa, sana güller verirdim<span id='a'><br>If dreams came true, I would give you roses</span></pre>"
++ "<pre>O güllerle belki de, kucağıma gelirdin<span id='a'><br>Maybe you would come into my arms with those roses</span></pre>"
++ "<pre>Rüyalar gerçek olsa, sana güller verirdim<span id='a'><br>If dreams came true, I would give you roses</span></pre>"
++ "<pre>O güllerle belki de, kucağıma gelirdin<span id='a'><br>Maybe you would come into my arms with those roses</span></pre><br>"
+
++ "<pre>Sarılırdım boynuna, sokulurdum koynuna<span id='a'><br>I would hug your neck, i would snuggle into your bosom</span></pre>"
++ "<pre>O gül dudaklarını öperdim doya doya<span id='a'><br>I would passionately kiss your rose-like lips</span></pre>"
++ "<pre>Sabah olmasın diye güneşi durdururdum<span id='a'><br>I would stop sun rising so that there would be no morning</span></pre>"
++ "<pre>Yanar dağlarda tüten ateşi söndürürdüm<span id='a'><br>I would extinguish the fire burning in the volcanoes</span></pre><br>"
+
++ "<pre>Rüyalar, rüyalar<span id='a'><br>Dreams, dreams</span></pre>"
++ "<pre>Rüyalaaar, rüyalar gerçek olsa<span id='a'><br>Dreeeaams, if dreams came true</span></pre><br>"
+
+document.getElementById("div_song03_lyrics").innerHTML = song03_lyrics;
+
+
+/*    Song04    */
+let song04_title = '<h3><strong>4.Şarkı - Bir Garip Yolcuyum</strong></h3>';
+document.getElementById("div_song04_title").innerHTML = song04_title;
+
+let song04_lyrics = "<pre>Bir garip yolcuyum, hayat yolunda<span id='a'><br>I'm a strange traveler in the path of life</span></pre>"
++ "<pre>Yolumu kaybetmiş, perişanım ben<span id='a'><br>I've lost my way, I'm in despair</span></pre>"
++ "<pre>Bir garip yolcuyum, hayat yolunda<span id='a'><br>I'm a strange traveler in the path of life</span></pre>"
++ "<pre>Yolumu kaybetmiş, perişanım ben<span id='a'><br>I've lost my way, I'm in despair</span></pre><br>"
+
++ "<pre>Mecnun misali, gurbet ellerde<span id='a'><br>Like Mecnun, in foreign lands</span></pre>"
++ "<pre>Ümitsiz sevginin, kurbanıyım ben<span id='a'><br>I'm the victim of hopeless love</span></pre>"
++ "<pre>Mecnun misali, gurbet ellerde<span id='a'><br>Like Mecnun, in foreign lands</span></pre>"
++ "<pre>Ümitsiz sevginin, kurbanıyım ben<span id='a'><br>I'm the victim of hopeless love</span></pre><br>"
+
++ "<pre>Yalan dünya, her şey bomboş<span id='a'><br>The world is a lie, everything is meaningless</span></pre>"
++ "<pre>Hancı sarhoş, yolcu sarhoş<span id='a'><br>The innkeeper is drunk, the traveler is drunk</span></pre>"
++ "<pre>Yalan dünya, her şey bomboş<span id='a'><br>The world is a lie, everything is meaningless</span></pre>"
++ "<pre>Hancı sarhoş, yolcu sarhoş<span id='a'><br>The innkeeper is drunk, the traveler is drunk</span></pre><br>"
+
++ "<p id='aranagme'>ARANAĞME<br><span></span></p><br>"
+
++ "<pre>Bir gün gibi sanki, geçti seneler<span id='a'><br>Years have passed like a single day</span></pre>"
++ "<pre>Ümidim kayboldu, perişanım ben<span id='a'><br>I've lost my hope, I'm in despair</span></pre>"
++ "<pre>Bir gün gibi sanki, geçti seneler<span id='a'><br>Years have passed like a single day</span></pre>"
++ "<pre>Ümidim kayboldu, perişanım ben<span id='a'><br>I've lost my hope, I'm in despair</span></pre><br>"
+
++ "<pre>Alın yazımmış, hayat yolunda<span id='a'><br>It is my fate, in the path of life</span></pre>"
++ "<pre>Ümitsiz sevginin, kurbanıyım ben<span id='a'><br>I'm the victim of hopeless love</span></pre>"
++ "<pre>Alın yazımmış, hayat yolunda<span id='a'><br>It is my fate, in the path of life</span></pre>"
++ "<pre>Ümitsiz sevginin, kurbanıyım ben<span id='a'><br>I'm the victim of hopeless love</span></pre><br>"
+
++ "<pre>Yalan dünya, her şey bomboş<span id='a'><br>The world is a lie, everything is meaningless</span></pre>"
++ "<pre>Hancı sarhoş, yolcu sarhoş<span id='a'><br>The innkeeper is drunk, the traveler is drunk</span></pre>"
++ "<pre>Yalan dünya, her şey bomboş<span id='a'><br>The world is a lie, everything is meaningless</span></pre>"
++ "<pre>Hancı sarhoş, yolcu sarhoş<span id='a'><br>The innkeeper is drunk, the traveler is drunk</span></pre><br>";
+
+document.getElementById("div_song04_lyrics").innerHTML = song04_lyrics;
+
+
+/*    Song05    */
+let song05_title = '<h3><strong>5.Şarkı - Dert Ortağım</strong></h3>';
+document.getElementById("div_song05_title").innerHTML = song05_title;
+
+let song05_lyrics = "<pre>Aşk kadehiyle içtiğim sensin<span id='a'><br>You are the one I drink with the cup of love</span></pre>"
++ "<pre>Sevgili diye seçtiğim sensin<span id='a'><br>You are the one I chose as my lover</span></pre>"
++ "<pre>Bütün dünyayı gezdim dolaştım<span id='a'><br>I've traveled all over the world</span></pre>"
++ "<pre>Anladım ki tek sevdiğim sensin<span id='a'><br>I realized that you are the only one I love</span></pre>"
++ "<pre>Bütün dünyayı gezdim dolaştım<span id='a'><br>I've traveled all over the world</span></pre>"
++ "<pre>Anladım ki tek sevdiğim sensin<span id='a'><br>I realized that you are the only one I love</span></pre><br>"
+
++ "<pre>Dert ortağım benim, biricik sevgilim<span id='a'><br>My confidant, my one and only love</span></pre>"
++ "<pre>Söyle senden başka, kimim var benim<span id='a'><br>Tell me, who else do I have besides you?</span></pre>"
++ "<pre>Dert ortağım benim, biricik sevgilim<span id='a'><br>My confidant, my one and only love</span></pre>"
++ "<pre>Söyle senden başka, kimim var benim<span id='a'><br>Tell me, who else do I have besides you?</span></pre><br>"
+
++ "<p id='aranagme'>ARANAĞME<br><span></span></p><br>"
+
++ "<pre>Senden uzakta geçmez günlerim<span id='a'><br>My days do not pass far away from you</span></pre>"
++ "<pre>Bir gün görmesem seni özlerim<span id='a'><br>If I don't see you for a day, I will miss you</span></pre>"
++ "<pre>Başka birini sevemem artık<span id='a'><br>I can't love anyone else anymore</span></pre>"
++ "<pre>Gelmesen bile seni beklerim<span id='a'><br>Even if you don't come, I'll wait for you</span></pre>"
++ "<pre>Başka birini sevemem artık<span id='a'><br>I can't love anyone else anymore</span></pre>"
++ "<pre>Gelmesen bile seni beklerim<span id='a'><br>Even if you don't come, I'll wait for you</span></pre><br>"
+
++ "<pre>Dert ortağım benim, biricik sevgilim<span id='a'><br>My confidant, my one and only love</span></pre>"
++ "<pre>Söyle senden başka, kimim var benim<span id='a'><br>Tell me, who else do I have besides you?</span></pre>"
++ "<pre>Dert ortağım benim, biricik sevgilim<span id='a'><br>My confidant, my one and only love</span></pre>"
++ "<pre>Söyle senden başka, kimim var benim<span id='a'><br>Tell me, who else do I have besides you?</span></pre>"
++ "<pre>Dert ortağım benim, biricik sevgilim<span id='a'><br>My confidant, my one and only love</span></pre>"
++ "<pre>Söyle senden başka, kimim var benim<span id='a'><br>Tell me, who else do I have besides you?</span></pre><br>";
+
+document.getElementById("div_song05_lyrics").innerHTML = song05_lyrics;
+
+
+/*    Song06    */
+let song06_title = '<h3><strong>6.Şarkı - Damarımda Kanımsın</strong></h3>';
+document.getElementById("div_song06_title").innerHTML = song06_title;
+
+let song06_lyrics = "<pre>Sen uzaklarda değil<span id='a'><br>You are not far away</span></pre>"
++ "<pre>Damarımda kanımsın<span id='a'><br>You are my blood in my veins</span></pre>"
++ "<pre>Ben sensiz yaşayamam<span id='a'><br>I can not live without you</span></pre>"
++ "<pre>Hayatımsın, canımsın<span id='a'><br>You are my life, you are my dear</span></pre>"
++ "<pre>Ben sensiz yaşayamam<span id='a'><br>I can not live without you</span></pre>"
++ "<pre>Hayatımsın, canımsın<span id='a'><br>You are my life, you are my dear</span></pre><br>"
+
++ "<pre>İste kölen olayım<span id='a'><br>Ask me to be your slave</span></pre>"
++ "<pre>İstersen öldür beni<span id='a'><br>Kill me if you want</span></pre>"
++ "<pre>Başkasını seversen<span id='a'><br>If you love someone else</span></pre>"
++ "<pre>Bil ki yaşatmam seni<span id='a'><br>Know that I will not let you live</span></pre><br>"
+
++ "<pre>İste kölen olayım<span id='a'><br>Ask me to be your slave</span></pre>"
++ "<pre>İstersen öldür beni<span id='a'><br>Kill me if you want</span></pre>"
++ "<pre>Başkasını seversen<span id='a'><br>If you love someone else</span></pre>"
++ "<pre>İnan yaşatmam seni<span id='a'><br>Believe me, I won't let you live</span></pre><br>"
+
++ "<p id='aranagme'>ARANAĞME<br><span></span></p><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>";
+
+document.getElementById("div_song06_lyrics").innerHTML = song06_lyrics;
+
+
+/*    Song07    */
+let song07_title = '<h3><strong>7.Şarkı - </strong></h3>';
+document.getElementById("div_song07_title").innerHTML = song07_title;
+
+let song07_lyrics = "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<p id='aranagme'>ARANAĞME<br><span></span></p><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>";
+
+document.getElementById("div_song07_lyrics").innerHTML = song07_lyrics;
+
+
+/*    Song08    */
+let song08_title = '<h3><strong>8.Şarkı - </strong></h3>';
+document.getElementById("div_song08_title").innerHTML = song08_title;
+
+let song08_lyrics = "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<p id='aranagme'>ARANAĞME<br><span></span></p><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>"
+
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre>"
++ "<pre><span id='a'><br></span></pre><br>";
+
+document.getElementById("div_song08_lyrics").innerHTML = song08_lyrics;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
